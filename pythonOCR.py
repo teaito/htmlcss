0097cb842f392d2d27ee487ab3441496f52698b3
teaito <teaitosvt@gmail.com>
2020-05-03 22:51:55 -1000

    suppression du fichier odt pour passer sur thonny Python

---
+++ pythonOCR.py
@@ -0,0 +1,49 @@
+//
+#division entière
+10 // 3 = 3
+
+%
+#modulo
+10 % 3 = 1
+
+#variable en minuscules, avec _ pour les espaces ou camelCase
+
+int
+#entier
+
+float
+#décimaux
+
+"""  phrase avec «  et ‘ """ 
+#triples guillemets pour encadrer une phrase sans avoir besoin d’échapper les guillemets et apostrophe et pour écire sur plusieurs lignes (ou 3 apostrophes)
+
+variable += 1
+#incrémente une variable de 1
+
+-= et *= et /=
+#soustraire, multiplier et diviser une variable par une valeur
+
+>>> a = 5
+>>> b = 32
+>>> a,b = b,a # permutation
+>>> a
+32
+>>> b
+5
+>>>
+
+x = y = 3
+#affectation à plusieurs variables en 1 fois
+
+type (variable)
+#pour obtenir le type d’une variable
+
+print (variable)
+#pour afficher
+
+si a > 0 :
+    print (« a est positif »)
+elif a = 0 :
+    print (« a est nul »)
+else :
+    print(« a est négatif »)
